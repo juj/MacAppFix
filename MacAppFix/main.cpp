e28249f86b465bb821b70eada07edf76661d136b
Jukka Jylanki <jukka.jylanki@ludocraft.com>
2012-03-20 12:01:44 +0200

    Initial Commit

---
+++ MacAppFix/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MacAppFix
+//
+//  Created by LudoCraft User on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
